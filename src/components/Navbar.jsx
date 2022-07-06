5f2b841d11c406fbeffdcb032c5b8c7f42ad8606
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-07-07 00:43:47 +0100

    Removed tailwind-css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 export default function Navbar () {
   return (
-    <nav class="m-10 text-center space-x-4">
-            <a class="px-4 py-2 text-white bg-black rounded-full
-            hover:rounded" href="/">Home</a>
-            <a class="px-4 py-2 text-white bg-black rounded-full
-            hover:rounded" href="about">About</a>
-            <a class="px-4 py-2 text-white bg-black rounded-full
-            hover:rounded" href="projects">Projects</a>
+    <nav>
+            <a href="/">Home</a>
+            <a href="about">About</a>
+            <a href="projects">Projects</a>
     </nav>
   )
 }
