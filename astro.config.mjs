b4c671582f9de6821834d234f25ef8da4f8fca70
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-08-31 16:18:45 +0300

    Fix: vercel build was not working properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import {defineConfig} from 'astro/config';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   adapter: vercel(),
+  output: "server",
 });
